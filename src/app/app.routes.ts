0e8ed499c91b889cd4232e0204af8dd26b0f9438
Aleksandr Tumanov <tumalexx02@gmail.com>
2024-10-14 01:09:10 +0500

    First day commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LayoutComponent} from './common-ui/layout/layout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: LayoutComponent
+  }
+];
